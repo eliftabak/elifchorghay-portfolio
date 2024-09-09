764a7956721cd36ab32d3bbb05b97b772e48a141
elif <elifchorghay@gmail.com>
2024-09-09 16:35:58 +0200

    Add style to navbar

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -1,27 +1,33 @@
-import Link from "next/link";
+"use client";
 
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
-	return (
+  const pathname = usePathname();
+
+  const isActive = (path: string) => pathname === path;
+
+  return (
     <nav className="hidden lg:block contain navbar sticky top-0 z-50 bg-white font-heading text-xl text-mdgray mt-4 px-3">
       <div className="flex justify-between py-4">
         <div className="navbar-brand">
-        <Link href="/" className="font-heading text-xl tracking-wider z-50 transition-colors duration-300">
-          Elif Chorghay
-        </Link>
+          <Link href="/" className="font-heading text-xl tracking-wider z-50 transition-colors duration-300">
+            Elif Chorghay
+          </Link>
         </div>
         <div className="navbar-menu">
           <div className="navbar-end flex gap-6">
-            <Link href="/about" className="navbar-item">
+            <Link href="/about" className={`navbar-item ${isActive("/about") ? "text-gray-600 font-bold border-b" : ""}`}>
               About
             </Link>
-            <Link href="/projects" className="navbar-item">
+            <Link href="/projects" className={`navbar-item ${isActive("/projects") ? "text-gray-600 font-bold border-b" : ""}`}>
               Projects
             </Link>
-            <Link href="/contact" className="navbar-item">
+            <Link href="/contact" className={`navbar-item ${isActive("/contact") ? "text-gray-600 font-bold border-b" : ""}`}>
               Contact
             </Link>
-            <Link href="/blog" className="navbar-item">
+            <Link href="/blog" className={`navbar-item ${isActive("/blog") ? "text-gray-600 font-bold border-b" : ""}`}>
               Blog
             </Link>
             <Link href="/ElifsResume.pdf" target="_blank" className="navbar-item">
